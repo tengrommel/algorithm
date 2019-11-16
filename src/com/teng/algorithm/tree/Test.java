9baadd3d44e8115d307d155b795ce23dd1a60ba6
zhouteng <wwwsousoucom@126.com>
2019-11-16 22:18:14 +0800

    树的介绍

---
+++ src/com/teng/algorithm/tree/Test.java
@@ -0,0 +1,11 @@
+package com.teng.algorithm.tree;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        // ArrayList底层也进行了扩容
+        ArrayList arrayList = new ArrayList();
+
+    }
+}
