4454ec7ee68e63dbbe7ad92a3c15bffb51088bd8
zhouteng <wwwsousoucom@126.com>
2019-11-18 18:39:40 +0800

    ok

---
+++ src/com/teng/algorithm/tree/HeapSort.java
@@ -0,0 +1,18 @@
+package com.teng.algorithm.tree;
+
+public class HeapSort {
+    public static void main(String[] args) {
+        // 要求进行升序排序
+        int arr[] = {4, 6, 8, 5, 9};
+    }
+
+    // 编写一个堆排序的方法
+    public static void heapSort(int arr[]) {
+        System.out.println("堆排序！！！");
+    }
+
+    // 将一个数组(二叉树)，调整成一个大顶堆
+    public static void addJustHeap(int arr[], int i,  int length) {
+
+    }
+}
